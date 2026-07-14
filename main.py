cf9aab7e8e03f692dd7afdcf8482e35d9a2797ef
Zakaria Mostafa <zakariamostafa015@gmail.com>
2026-07-14 15:16:52 +0300

    feat: move routes to seperate files + load .env values

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 from fastapi import FastAPI
-app = FastAPI()
+from dotenv import load_dotenv
+load_dotenv(".env")
+from routes import base
 
-@app.get("/welcome")
-def welcome():
-    return{
-        "message": "Hellow World!"
-    }
+app = FastAPI()
+app.include_router(base.base_router)
