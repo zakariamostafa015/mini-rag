87934874db40a368622f4745f9efe7d19611e205
Zakaria Mostafa <zakariamostafa015@gmail.com>
2026-08-02 16:22:33 +0300

    chore: update main.py to include NLP routes and correct LLM provider settings

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from routes import base, data
+from routes import base, data, nlp
 from motor.motor_asyncio import AsyncIOMotorClient
 from helpers.config import Settings, get_settings
 from stores.llm.LLMProviderFactory import LLMProviderFactory
@@ -16,13 +16,13 @@ async def startup_span():
     vector_db_provider_factory = VectorDBProviderFactory(config=settings)
 
     # generation client
-    app.state.llm_generation_client = llm_provider_factory.create(provider=settings.LLM_GENERATION_PROVIDER)
-    app.state.llm_generation_client.set_generation_model(model_id= settings.LLM_GENERATION_MODEL_ID)
+    app.state.llm_generation_client = llm_provider_factory.create(provider=settings.GENERATION_BACKEND)
+    app.state.llm_generation_client.set_generation_model(model_id= settings.GENERATION_MODEL_ID)
 
     # embedding client
-    app.state.llm_embedding_client = llm_provider_factory.create(provider=settings.LLM_EMBEDDING_PROVIDER)
-    app.state.llm_embedding_client.set_embedding_model(model_id= settings.LLM_EMBEDDING_MODEL_ID,
-                                                        emembedding_size= settings.LLM_EMBEDDING_MODEL_SIZE)
+    app.state.llm_embedding_client = llm_provider_factory.create(provider=settings.EMBEDDING_BACKEND)
+    app.state.llm_embedding_client.set_embedding_model(model_id= settings.EMBEDDING_MODEL_ID,
+                                                        embedding_size= settings.EMBEDDING_MODEL_SIZE)
 
     # vector db client
     app.state.vector_db_client = vector_db_provider_factory.create(provider=settings.VECTOR_DB_BACKEND)
@@ -32,8 +32,12 @@ async def shutdown_span():
     app.state.mongodb_conn.close()
     app.state.vector_db_client.disconnect()
 
-app.router.lifespan.on_startup.append(startup_span)
-app.router.lifespan.on_shutdown.append(shutdown_span)
+# app.router.lifespan.on_startup.append(startup_span)
+# app.router.lifespan.on_shutdown.append(shutdown_span)
+
+app.on_event("startup")(startup_span)
+app.on_event("shutdown")(shutdown_span)
 
 app.include_router(base.base_router)
 app.include_router(data.data_router)
+app.include_router(nlp.nlp_router)
