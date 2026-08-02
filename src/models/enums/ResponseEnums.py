f5362c03b51e1ce64df13bfcfefedc8fa13b6b0e
Zakaria Mostafa <zakariamostafa015@gmail.com>
2026-08-02 16:22:55 +0300

    feat: add new response signals for vector DB operations in ResponseEnums

--- src/models/enums/ResponseEnums.py
+++ src/models/enums/ResponseEnums.py
@@ -13,3 +13,10 @@ class ResponseSignal(Enum):
 
     NO_FILES_ERROR = "not_found_files"
     FILE_ID_ERROR = "no_file_found_with_this_id"
+
+    PROEJECT_NOT_FOUND_ERROR = "project_not_found"
+    INSERT_INTO_VECTOR_DB_FAILED = "insert_into_vector_db_failed"
+    INSERT_INTO_VECTOR_DB_SUCCESS = "insert_into_vector_db_success"
+    VECTORDB_COLLECTION_INFO_RETRIVED = "vector_db_collection_info_retrived"
+    VECTORDB_SEARCH_SUCCESS = "vector_db_search_success"
+    VECTORDB_SEARCH_FAILED = "vector_db_search_failed"
