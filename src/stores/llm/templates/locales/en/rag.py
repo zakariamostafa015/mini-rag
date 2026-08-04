1b8cb7db616f458cfbd250ed5ab23c46bc78eb55
Zakaria Mostafa <zakariamostafa015@gmail.com>
2026-08-04 15:34:58 +0300

    feat: add template files and TemplateParser for multilingual support

---
+++ src/stores/llm/templates/locales/en/rag.py
@@ -0,0 +1,36 @@
+from string import Template
+
+#### RAG Prompt #####
+#### System ####
+
+system_prompt = Template(
+    "\n".join([
+        "You are an assistant to generate a response for the user.",
+        "You will be provided by a set of documents associated with the user's query.",
+        "You have to generate a response based on the documents provided.",
+        "Ignore the documents that are not relevant to the user's query.",
+        "You can apologize to the user if you are not able to generate a response.",
+        "You have to generate response in the same language as the user's query.",
+        "Be polite and respectful to the user.",
+        "Be precise and concise in your response. Avoid unnecessary information."
+    ])
+)
+
+
+### Document Prompt ###
+
+document_prompt = Template(
+    "\n".join([
+        "## Document: $doc_num",
+        "### Content: $chunk_text"
+    ])
+) 
+
+
+### Footer 
+
+footer_prompt = Template(
+    "\n".join({
+        "Based only on the above documents, please generate an answer for the user"
+    })
+)
