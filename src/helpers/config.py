e946a7a3cef0775a288d83c26c7053640fc36a48
Zakaria Mostafa <zakariamostafa015@gmail.com>
2026-08-04 15:36:21 +0300

    feat: add PRIMARY_LANGUAGE and DEFAULT_LANGUAGE to configuration settings

--- src/helpers/config.py
+++ src/helpers/config.py
@@ -32,6 +32,9 @@ class Settings(BaseSettings):
     VECTOR_DB_PATH: str
     VECTOR_DB_DISTANCE_METHOD: str = None
 
+    PRIMARY_LANGUAGE: str = "en"
+    DEFAULT_LANGUAGE: str = "en"
+    
     class Config:
         env_file = ".env"
 
