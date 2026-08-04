1bafb584c8bc35a1ffba6ee27564ccb00c7dd99a
Zakaria Mostafa <zakariamostafa015@gmail.com>
2026-08-04 17:15:51 +0300

    feat: enhance footer prompts in RAG templates to include question and answer sections

--- src/controllers/NLPController.py
+++ src/controllers/NLPController.py
@@ -125,7 +125,9 @@ class NLPController(BaseController):
             for idx, doc in enumerate(retrieved_documents)
         ])
 
-        footer_prompt = self.template_parser.get("rag","footer_prompt")
+        footer_prompt = self.template_parser.get("rag","footer_prompt", { 
+            "query": query 
+        })
 
         chat_history = [
             self.generation_client.construct_prompt(
--- src/stores/llm/templates/locales/en/rag.py
+++ src/stores/llm/templates/locales/en/rag.py
@@ -31,6 +31,10 @@ document_prompt = Template(
 
 footer_prompt = Template(
     "\n".join({
-        "Based only on the above documents, please generate an answer for the user"
+        "Based only on the above documents, please generate an answer for the user",
+        "## Question:",
+        "$query",
+        "",
+        "## Answer:"
     })
 )
