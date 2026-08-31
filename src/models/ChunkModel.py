f3df90140743cc1fe58476ffdbf41e5052d7e556
Zakaria Mostafa <zakariamostafa015@gmail.com>
2026-08-31 12:29:22 +0300

    feat: add methods to get total chunk count and enhance text embedding support in CoHere and OpenAI providers

--- src/models/ChunkModel.py
+++ src/models/ChunkModel.py
@@ -58,3 +58,12 @@ class ChunkModel(BaseDataModel):
             result = await session.execute(stmt)
             await session.commit()
         return result.rowcount
+
+    async def get_total_chunks_count(self, project_id: ObjectId):
+        total_count = 0
+        async with self.db_client() as session:
+            count_sql = select(func.count(DataChunk.chunk_id)).where(DataChunk.chunk_project_id == project_id)
+            records_count = await session.execute(count_sql)
+            total_count = records_count.scalar()
+
+        return total_count
